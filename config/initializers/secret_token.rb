c783e0fc54598ca49ca4a170fdf96116225e5f1e
Lukas Diacik <diacik@easysoftware.cz>
2012-09-16 21:04:13 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sonny::Application.config.secret_token = 'c09bc3045d4b5cfd97f16e2766e840b64c521a569455a366d5a20c00e9fac53af8176a8f6abea5efc19317c17de7203270f697c812050123d73ba69d9326d9b0'
